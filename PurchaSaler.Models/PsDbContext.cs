c7b9e830806458d4312b9cdcd3879f547a780499
ulquiola <ulquiola@163.com>
2019-12-12 11:08:50 +0800

    添加项目文件。

---
+++ PurchaSaler.Models/PsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace PurchaSaler.Models
+{
+    public class PsDbContext:DbContext
+    {
+        public PsDbContext(DbContextOptions<PsDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ PurchaSaler.Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PurchaSaler.Models
+{
+    public class Users
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+}
