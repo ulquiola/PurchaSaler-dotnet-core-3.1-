069cfe35fd5827e4f5cf04d72ce885680b6c0030
ulquiola <ulquiola@163.com>
2019-12-17 08:10:27 +0800

    preview

--- PurchaSaler.Models/Users.cs
+++ PurchaSaler.Models/Users.cs
@@ -12,5 +12,8 @@ namespace PurchaSaler.Models
 
         public string UserName { get; set; }
 
+        public string PassWord { get; set; }
+
+
     }
 }
--- PurchaSaler.Web/Controllers/HomeController.cs
+++ PurchaSaler.Web/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PurchaSaler.Models;
 using PurchaSaler.Web.Models;
 
 namespace PurchaSaler.Web.Controllers
@@ -12,15 +13,25 @@ namespace PurchaSaler.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PsDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            PsDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Users users)
+        {
+            _db.Users.Add(users);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
